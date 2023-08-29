995b145217c72e732cbda1c08fb958bb81872534
flowercloud <55479839+flowercloud2023@users.noreply.github.com>
2023-08-29 10:17:02 +0800

    Delete setup_package.py

--- setup_package.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-# python setup_package.py build_ext --inplace
-setup(
-    name='bee',
-    ext_modules=cythonize("aunit.py"),
-)
